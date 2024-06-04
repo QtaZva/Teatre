0df7e3c4571e3c750b88422c038f48b6b1c8f00d
QtaZva <qtazva@mail.ru>
2024-06-04 11:16:04 +0900

    fix

---
+++ Teatre/Windows/AddProductionsWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Teatre.Classes;
+
+namespace Teatre.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddProductionsWindow.xaml
+    /// </summary>
+    public partial class AddProductionsWindow : Window
+    {
+        ApplicationContext db;
+        public AddProductionsWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddProductions(object sender, RoutedEventArgs e)
+        {
+            db = new ApplicationContext();
+            Productions newProduction = new Productions(Convert.ToInt32(roleId.Text), Convert.ToInt32(actorId.Text), appointmentDate.Text, withdrawalDate.Text);
+        }
+    }
+}
